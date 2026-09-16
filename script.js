b47bf5f9b4868d7de9aa18037019acc36e78faeb
Lotus Hosting <planetajoaotazz@gmail.com>
2026-09-16 19:44:48 -0300

    Update link for mosca in irParaSite function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function irParaSite(vetor) {
   const links = {
-    mosca: "https://meninotazz.github.io/mosca/",
+    mosca: "https://meninotazz.github.io/Jornada-Cientifica-CESFA-M14B/mosca/",
     barata: "https://meninotazz.github.io/Barata-F/",
     rato: "https://meninotazz.github.io/Rato-F/"
   };

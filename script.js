f5e2f475c1eb8f3dd9cffe9cb454f0ee8ca739ed
Lotus Hosting <planetajoaotazz@gmail.com>
2026-09-16 16:19:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function irParaSite(vetor) {
+  const links = {
+    mosca: "https://meninotazz.github.io/Mosca-F/",
+    barata: "https://meninotazz.github.io/Barata-F/",
+    rato: "https://meninotazz.github.io/Rato-F/"
+  };
+
+  if (links[vetor]) {
+    window.open(links[vetor], "_blank");
+  }
+}

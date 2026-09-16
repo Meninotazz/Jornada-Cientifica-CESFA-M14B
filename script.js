85a7e13a1a47e6dc24c22d50b98b34d6eb819d8c
Lotus Hosting <planetajoaotazz@gmail.com>
2026-09-16 19:34:17 -0300

    Fix URL case sensitivity in irParaSite function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function irParaSite(vetor) {
   const links = {
-    mosca: "https://meninotazz.github.io/Mosca-F/",
+    mosca: "https://meninotazz.github.io/mosca/",
     barata: "https://meninotazz.github.io/Barata-F/",
     rato: "https://meninotazz.github.io/Rato-F/"
   };
